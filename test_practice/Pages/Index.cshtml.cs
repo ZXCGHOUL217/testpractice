79ae43753971c805a3872ef86e9f645eb400e0c1
ZXCGHOUL217 <voinkam@bk.ru>
2025-12-22 13:04:24 +0300

    test

--- test_practice/Pages/Index.cshtml.cs
+++ test_practice/Pages/Index.cshtml.cs
@@ -1,20 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using test_practice.Data;
+using test_practice.Models;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        //datas
+        public List<Product> FeaturedProducts { get; set; } = new();
+        public List<Category> Categories { get; set; } = new();
+
+        public IndexModel(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            Categories = await _context.Categories.ToListAsync();
 
+            FeaturedProducts = await _context.Products
+                .Include(p => p.Category)
+                .Take(6)
+                .ToListAsync();
         }
     }
 }
